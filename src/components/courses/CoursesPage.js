75a27e774a31ab6c6584dd8720dd08d405fcf257
akmal <akmal@brighter.com>
2016-08-15 18:46:54 -0700

    add a form and link state changes are event submitters

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+	constructor(props, context){
+		super(props, context);
+
+		this.state = {
+			course : {title : ""}
+		};
+
+
+		this.onTitleChange = this.onTitleChange.bind(this);
+		this.onClickSave = this.onClickSave.bind(this);
+	}
+
+	onTitleChange(event) {
+		const course = this.state.course;
+		course.title = event.target.value;
+		this.setState({course : course});
+	}
+
+	onClickSave(event) {
+		alert(`Saving ${this.state.course.title}`);
+	}
+
+
 	render(){
 		return (
 			<div>
 				<h1>Courses</h1>
-				<p>Here goes the courses page</p>
+				<h2>Add Course</h2>
+				<input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
+
+				<input type="submit" value="Save" onClick={this.onClickSave} />
 			</div>
 		);
 	}
