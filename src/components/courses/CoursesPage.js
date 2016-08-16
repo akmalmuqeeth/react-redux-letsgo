a56de26443d8aa7752bb63a63015814e67210000
akmal <akmal@brighter.com>
2016-08-15 17:45:55 -0700

    add the Courses page

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CoursesPage extends React.Component {
+	render(){
+		return (
+			<div>
+				<h1>Courses</h1>
+				<p>Here goes the courses page</p>
+			</div>
+		);
+	}
+}
+
+export default CoursesPage;
